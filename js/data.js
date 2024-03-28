694cd3a08681a11d1cdd684919c94eae22238a76
itsjackie00 <jackiedanso33@gmail.com>
2024-03-28 18:16:03 +0100

    added export

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
  export const slides = [
-    {
+        {
         image: 'img/01.webp',
             title: 'Marvel\'s Spiderman Miles Morale',
             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
